6956e291ea788c70a489716be0f30601389c55c5
aknurtolymbek <aknurtolymbek042@gmail.com>
2025-11-09 21:16:47 +0500

    ready footers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,12 @@ import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import './styles/Global.scss'
 import Home from "./pages/test/Home";
+import HeaderBlack from "./components/Header/HeaderBlack";
+import HeaderWhite from "./components/Header/HeaderWhite";
+import FooterBlack from "./components/Footer/FooterBlack";
+import FooterWhite from "./components/Footer/FooterWhite";
+import Card1 from "./components/Cards/Card1";
+
 
 
 const PrivateRoute = ({ children }) => {
@@ -14,6 +20,11 @@ const PrivateRoute = ({ children }) => {
 const App = () => {
     return (
         <Routes>
+            <Route path="/card1" component={Card1} />
+            <Route path = "/footerWhite" element={<FooterWhite />} />
+            <Route path = "/footerBlack" element={<FooterBlack />} />
+            <Route path="/headerBlack" element={<HeaderBlack />} />
+            <Route path="/headerWhite" element={<HeaderWhite />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route
